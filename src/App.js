af7c46f6c915036d35bfa4c23d5ea6be8ef484ae
HP-dufeng <fengdu_xiangyang@163.com>
2017-02-16 14:11:49 +0800

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Header, Card, CardSection, Button } from './components/common';
+
+
+const App = () => {
+    return (
+        <View>
+            <Header headerText='Auth' />
+        </View>
+    );
+};
+
+export default App;
